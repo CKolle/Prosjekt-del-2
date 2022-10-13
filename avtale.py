024abe308fd4b2df77e8d1407bbd065359a6cfcc
Kjell <kjelli.jossang2@gmail.com>
2022-10-13 19:12:43 +0200

    Fikset noen typos og la til oppgavetekst

--- avtale.py
+++ avtale.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-class Avatale:
+class Avtale:
     def __init__(self, tittel: str, sted: str, varighet_min: int, starttidspunkt: datetime ) -> None:
-        self.tittle = tittel
+        self.tittel = tittel
         self.sted = sted
         self.varighet_min = varighet_min
         self.starttidspunkt = starttidspunkt
