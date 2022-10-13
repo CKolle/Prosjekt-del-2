857771d0098ed718448330cc3b909ef7b245e313
CKolle <cl.kolle@stud.uis.no>
2022-10-13 13:46:43 +0200

    Gjorde oppgave d

--- avatale.py
+++ avatale.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 class Avatale:
     def __init__(self, tittel: str, sted: str, varighet_min: int, starttidspunkt: datetime ) -> None:
         self.tittle = tittel
