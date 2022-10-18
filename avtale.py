14c4793547ec6c12eaab4ca3e39a7ee983833542
CKolle <cl.kolle@stud.uis.no>
2022-10-18 13:34:54 +0200

    La til typing for for rutinen utskrift_avtaler.

--- avtale.py
+++ avtale.py
@@ -1,23 +1,26 @@
 from datetime import datetime
 
+
 class Avtale:
-    def __init__(self, tittel: str, sted: str, varighet_min: int, starttidspunkt: datetime ) -> None:
+    def __init__(self, tittel: str, sted: str, varighet_min: int, starttidspunkt: datetime) -> None:
         self.tittel = tittel
         self.sted = sted
         self.varighet_min = varighet_min
         self.starttidspunkt = starttidspunkt
 
     def __str__(self):
-        return f'Avtale: {self.tittel}\nSted: {self.sted}\nVarighet: {self.varighet_min} min\nDato: {self.starttidspunkt}'
+        return f"Avtale: {self.tittel}\nSted: {self.sted}\nVarighet: {self.varighet_min} min\nDato: {self.starttidspunkt}"
 
-def lag_avtale()-> Avtale:
+
+def lag_avtale() -> Avtale:
     """Lager et gyldig avtale objekt"""
 
     tittel = input("Vennligst oppgi navnet på tittelen på avtalen: ")
     sted = input("Vennligst oppgi stedet for avtalen: ")
     while True:
         try:
-            varighet_min = int(input("Vengligst oppgi hvor lenge avtalen varer (minutter): ").replace(",","."))
+            varighet_min = int(
+                input("Vengligst oppgi hvor lenge avtalen varer (minutter): ").replace(",", "."))
         except ValueError:
             print("Ugyldig tall, prøv igjen")
             continue
@@ -27,22 +30,24 @@ def lag_avtale()-> Avtale:
         break
 
     while True:
-        dato = input("Vennligst oppgi dato for avtalen (DD.MM.ÅÅÅÅ): ").split(".")
+        dato = input(
+            "Vennligst oppgi dato for avtalen (DD.MM.ÅÅÅÅ): ").split(".")
         try:
             dag, maanede, aar = dato
             dag = int(dag)
             maanede = int(maanede)
             aar = int(aar)
 
-            #Ser om det er en gyldig dato
+            # Ser om det er en gyldig dato
             datetime(aar, maanede, dag)
         except ValueError:
             print("Vennligst oppgi en gyldig dato")
             continue
-        break 
+        break
 
     while True:
-        klokkeslett = input("Vengligst oppi et klokkelsett for avtalen (TT:MM): ").split(":")
+        klokkeslett = input(
+            "Vengligst oppi et klokkelsett for avtalen (TT:MM): ").split(":")
         try:
             time, minutt = klokkeslett
             time = int(time)
@@ -55,16 +60,17 @@ def lag_avtale()-> Avtale:
 
     return Avtale(tittel, sted, varighet_min, starttidspunkt)
 
-def utskrift_avtaler(avtaler:list, overskrift=""):
+
+def utskrift_avtaler(avtaler: list[Avtale], overskrift=""):
     if overskrift != "":
         print(overskrift)
-    for i in avtaler:
-        print(f'{avtaler.index(i)}: {i.tittel}')
+    for avtale in avtaler:
+        print(f"{avtaler.index(avtale)}: {avtale.tittel}")
 
 
 def main():
     """Inngangen til programmet"""
 
+
 if __name__ == "__main__":
     main()
-    
