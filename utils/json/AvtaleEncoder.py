d47950d345550b14b5ddf745cc30f5c686869409
CKolle <cl.kolle@stud.uis.no>
2022-11-01 13:29:58 +0100

    Flyttet logikken for decoding av avtale objekt

---
+++ utils/json/AvtaleEncoder.py
@@ -0,0 +1,9 @@
+
+from json import JSONEncoder
+from typing import Any
+
+
+class AvtaleEncoder(JSONEncoder):
+    def default(self, o: Any) -> Any:
+        o.starttidspunkt = str(o.starttidspunkt)
+        return o.__dict__
