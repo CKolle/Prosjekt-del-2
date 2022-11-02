e8fef43bda29f1a8e17b4e659b0ea3cae75e0eb5
CKolle <cl.kolle@stud.uis.no>
2022-11-02 11:18:26 +0100

    Byttet import for å være konsekevent

--- src/utils/json/KategoriEncoder.py
+++ src/utils/json/KategoriEncoder.py
@@ -1,8 +1,8 @@
-import json
+from json import JSONEncoder
 from typing import Any
 
 
-class KategoriEncoder(json.JSONEncoder):
+class KategoriEncoder(JSONEncoder):
     def default(self, o: Any) -> Any:
         o.prioritet = o.prioritet.value
         return o.__dict__
