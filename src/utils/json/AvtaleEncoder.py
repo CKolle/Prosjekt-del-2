789cca49c440693e1893599e12ef035f1b9a6d17
CKolle <cl.kolle@stud.uis.no>
2022-11-10 17:13:23 +0100

    Encoderen håndterer også nå kategori og en sted it

--- src/utils/json/AvtaleEncoder.py
+++ src/utils/json/AvtaleEncoder.py
@@ -1,8 +1,14 @@
 from json import JSONEncoder
 from typing import Any
+from utils.json.KategoriEncoder import KategoriEncoder
 
 
 class AvtaleEncoder(JSONEncoder):
     def default(self, o: Any) -> Any:
         o.starttidspunkt = str(o.starttidspunkt)
+
+        o.kategorier = [] if o.kategorier is None else [
+            kategori.id for kategori in o.kategorier]
+
+        o.sted = o.sted.id
         return o.__dict__
